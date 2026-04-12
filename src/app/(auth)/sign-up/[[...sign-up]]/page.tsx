f1e695d6ea8d47c14068a49bb806c3f4d5caeef7
JasonLeviGoodison <jasongoodison1234@gmail.com>
2026-04-12 11:43:51 -0700

    Fixing sign up flow

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <SignUp
-      afterSignUpUrl="/dashboard"
+      signUpFallbackRedirectUrl="/dashboard"
       signInUrl="/sign-in"
     />
   );
